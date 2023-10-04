d6aa0b31cde1eb6a90b0d7e15bba5312733552f4
ronaulisijabat <ronaulisijabat25@gmail.com>
2023-10-04 17:54:07 +0700

    menambahkan folder dan latihan pt. 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya ronauli sijabat <br>
+        saya tinggal di daerah srengseng <br>
+        olah raga yang saya sukai adalah berenang";
+    }
+}
