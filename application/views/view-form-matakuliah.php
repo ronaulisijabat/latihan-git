844304afac8fcd2c5763b5dd29b4b5d9510a6c60
ronaulisijabat <ronaulisijabat25@gmail.com>
2023-10-16 15:38:40 +0700

    menambahkan tugas pert 05

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -6,7 +6,8 @@
 
 <body>
  <center>
- <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+    <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+    <?php echo validation_errors()?>
            <table>
                 <tr>
                     <th colspan="3">
